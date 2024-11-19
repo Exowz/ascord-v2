d1c3b3e2bfe0f344c59827e6ebc7cedfca97fa5a
Exowz <53607938+Exowz@users.noreply.github.com>
2024-11-19 19:06:11 +0100

    setting up Auth functionnality

---
+++ schemas/index.ts
@@ -0,0 +1,29 @@
+import * as z from "zod";
+
+export const LoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+export const RegisterSchema = z
+  .object({
+    firstName: z
+      .string()
+      .min(1, { message: "First Name is required" }),
+    lastName: z
+      .string()
+      .min(1, { message: "Last Name is required" }),
+    email: z
+      .string()
+      .email({ message: "Invalid email address" }),
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters" }),
+    confirmPassword: z
+      .string()
+      .min(8, { message: "Confirm Password must be at least 8 characters" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"], // Where to apply the error
+    message: "Passwords do not match",
+  });
