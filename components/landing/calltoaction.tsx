01de5cc66fc5d14ba2f2e21cdf5bf7ce76b558f6
Exowz <53607938+Exowz@users.noreply.github.com>
2024-11-30 21:29:01 +0100

    landing page done

--- components/landing/calltoaction.tsx
+++ components/landing/calltoaction.tsx
@@ -27,7 +27,7 @@ export default function CallToAction() {
     <section className="py-12">
          <div className="flex flex-col items-center justify-center h-[40rem]  ">
       <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
-        The road to freedom starts from here
+        Ready to Get Started?
       </p>
       <TypewriterEffectSmooth words={words} />
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
